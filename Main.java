797e733772ac1c86a8b54a5d9562da301d396b53
PavelGozhii <36234557+PavelGozhii@users.noreply.github.com>
2019-04-07 13:45:29 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,12 +2,8 @@ package mate.academy;
 
 import mate.academy.di.Injector;
 
-import java.io.IOException;
-
 public class Main {
-    public static void main(String[] args) throws IOException, IllegalAccessException {
+    public static void main(String[] args) throws IllegalAccessException {
         Injector.create();
     }
-
-
 }
