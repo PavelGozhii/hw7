9dce3958fec1f93da5528728415f34005c56fac1
PavelGozhii <36234557+PavelGozhii@users.noreply.github.com>
2019-04-07 13:39:57 +0300

    Update ClientConsoleHandler.java

--- ClientConsoleHandler.java
+++ ClientConsoleHandler.java
@@ -7,15 +7,15 @@ import mate.academy.model.Client;
 
 import java.util.Scanner;
 
-public class ClientConsoleHandler implements ConsoleHandler{
+public class ClientConsoleHandler implements ConsoleHandler {
 
     @Inject
     private static Dao clientDao;
 
-    public static void handle() throws IllegalAccessException {
+    public void handle() throws IllegalAccessException {
         while (true) {
             Scanner scanner = new Scanner(System.in);
-            System.out.println("1 - если вносите клиента, 2 - если получаете клиента, 3 - для выхода");
+            System.out.println("1 - for enter client, 2 - for get clien, 3 - return back");
             int consoleChoice = scanner.nextInt();
             switch (consoleChoice) {
                 case 1:
@@ -31,11 +31,11 @@ public class ClientConsoleHandler implements ConsoleHandler{
     }
 
 
-    private static void addClientInfo(Scanner scanner) {
-        System.out.println("Введите информацию о клиенте");
-        System.out.println("Введите имя");
+    private void addClientInfo(Scanner scanner) {
+        System.out.println("Enter info about client");
+        System.out.println("Enter name");
         String name = scanner.next();
-        System.out.println("Введите телефон");
+        System.out.println("Enter phone");
         String phone = scanner.next();
         Client client = new Client(name, phone);
         clientDao.save(client);
@@ -43,3 +43,4 @@ public class ClientConsoleHandler implements ConsoleHandler{
 
 
 }
+
